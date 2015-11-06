2554cc7a0f9d6bb9d4e2ce092ac4773226f88328
Anna-Rader <annabrader@gmail.com>
2015-11-06 12:28:35 -0700

    Updated waterbug
    Updated waterbug changes for Wyoming Public Radio.

--- www/js/waterbug-config.js
+++ www/js/waterbug-config.js
@@ -5,20 +5,20 @@ var elementPadding = 40; // padding around the logo and credit text
 // logo configuration
 // the name of the logo object should match the value of the corresponding radio button in the HTML.
 var logos = {
-    'lunchbox': {
-        whitePath: '../img/icon-lunchbox-white.svg', // path to white logo
-        blackPath: '../img/icon-lunchbox-black.svg', // path to black logo
+    'WYPublicRadio': {
+        whitePath: '../img/Bison_White_120.png', // path to white logo
+        blackPath: '../img/Bison_Blue_120.png', // path to black logo
         w: 100, // width of logo
-        h: 80, // height of logo
-        display: 'Lunchbox'
+        h: 55, // height of logo
+        display: 'Wyoming Public Radio'
     },
-    'socializr': {
-        whitePath: '../img/icon-socializr-white.svg',
-        blackPath: '../img/icon-socializr-black.svg',
-        w: 150,
-        h: 51,
-        display: 'Socializr'
-    }
+    //'socializr': {
+        //whitePath: '../img/icon-socializr-white.svg',
+        //blackPath: '../img/icon-socializr-black.svg',
+        //w: 150,
+        //h: 51,
+        //display: 'Socializr'
+    //}
 };
 
 // logo opacity for colors
@@ -28,14 +28,14 @@ var blackLogoAlpha = '0.6';
 // type
 var fontWeight = 'normal'; // font weight for credit
 var fontSize = '20pt'; // font size for credit
-var fontFace = "Helvetica"; // font family for credit
+var fontFace = "Lato"; // font family for credit
 var fontShadow = 'rgba(0,0,0,0.7)'; // font shadow for credit
 var fontShadowOffsetX = 0; // font shadow offset x
 var fontShadowOffsetY = 0; // font shadow offset y
 var fontShadowBlur = 10; // font shadow blur
 
 // copyright options
-var orgName = 'Your News Organization';
+var orgName = 'Wyoming Public Radio';
 var freelanceString = 'for ' + orgName;
 
 var copyrightOptions = {
@@ -55,21 +55,21 @@ var copyrightOptions = {
         source: freelanceString,
         display: 'Freelance' 
     },
-    'ap': {
+    'Flickr': {
         showPhotographer: true,
         showSource: false,
-        photographerRequired: false,
+        photographerRequired: true,
         sourceRequired: false,
-        source: 'AP',
-        display: 'AP' 
+        source: 'Flickr Creative Commons',
+        display: 'Flickr' 
     },
-    'getty': {
+    'IE': {
         showPhotographer: true,
         showSource: false,
         photographerRequired: false,
         sourceRequired: false,
-        source: 'Getty Images',
-        display: 'Getty' 
+        source: 'Inside Energy',
+        display: 'IE' 
     },
     'thirdParty': {
         showPhotographer: true,
@@ -83,7 +83,7 @@ var copyrightOptions = {
 
 // app load defaults
 var currentCrop = 'twitter'; // default crop size
-var currentLogo = 'lunchbox'; // default logo slug
+var currentLogo = 'WyPublicRadio'; // default logo slug
 var currentLogoColor = 'white'; // default logo color
 var currentTextColor = 'white'; // default text color
 var defaultImage = '../img/test-kitten.jpg'; // path to image to load as test image
